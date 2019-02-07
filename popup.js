793c7284f4cbf6fd013d44e8011c21f87b1d181d
JuliaNeumann <jn.julianeumann@gmail.com>
2019-02-07 09:37:04 +0100

    same tab, just one button, removes bookmarks from list

--- popup.js
+++ popup.js
@@ -1,29 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     let startButton = document.getElementById("start");
-    let nextButton = document.getElementById("next");
 
     startButton.addEventListener('click', function() {
-        chrome.bookmarks.getTree(
-            function(bookmarkTreeNodes) {
-                // 38 is in this case my readMe folder, TODO: get by iteration through all
-                nextButton.disabled = false;
-                startButton.disabled = true;
-                handleSeenUrl(bookmarkTreeNodes);
-                //chrome.tabs.create({'url': bookmarkTreeNodes[0].children[0].children[38].children[0].url});
-                // TODO: store opened url & remove from bookmarks list
-                // TODO: add next button - popup?
-            });
+        chrome.bookmarks.getTree(handleNextBookmark)
     }, false);
 }, false);
 
-function handleSeenUrl(bookmarkTreeNodes) {
-    chrome.storage.sync.get(["readMeSeenUrls"], function(result) {
-        let seenUrls = result.seenUrls || [];
-        seenUrls.push(bookmarkTreeNodes[0].children[0].children[38].children[0].url);
-        chrome.storage.sync.set({"readMeSeenUrls": {seenUrls}}, function() {
-            chrome.storage.sync.get(["readMeSeenUrls"], function(result) {
-               console.log(result);
-            });
+function handleNextBookmark(bookmarkTreeNodes) {
+    chrome.storage.sync.get(["readMe"], function(result) {
+        let seenUrls = result.readMe && result.readMe.seenUrls ? result.readMe.seenUrls : [];
+        // 38 is in this case my readMe folder, TODO: get by iteration through all
+        let nextBookmark = bookmarkTreeNodes[0].children[0].children[38].children[0];
+        seenUrls.push(nextBookmark);
+        chrome.storage.sync.set({"readMe": {seenUrls}}, function() {
+            chrome.tabs.update({'url': nextBookmark.url});
+            chrome.bookmarks.remove(nextBookmark.id);
         });
     });
 }
